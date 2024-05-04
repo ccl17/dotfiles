fa6a5db5daa4bb9db344fe8621651928a6f19404
Samuel <chenlu.ccl@gmail.com>
2024-05-04 23:42:45 +0800

    disable lazy load treesitter

--- neovim/.config/nvim/lua/plugins/treesitter.lua
+++ neovim/.config/nvim/lua/plugins/treesitter.lua
@@ -4,6 +4,7 @@ return {
     'nvim-treesitter/nvim-treesitter-textobjects',
   },
   event = { 'BufReadPost', 'BufNewFile' },
+  lazy = false,
   config = function()
     require('nvim-treesitter.configs').setup({
       ensure_installed = {
