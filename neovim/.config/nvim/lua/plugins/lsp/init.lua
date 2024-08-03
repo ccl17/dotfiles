1c7276b9188149fcb521183411da655912c92d2d
Samuel <chenlu.ccl@gmail.com>
2024-08-03 15:41:59 +0800

    remove unnecessary semantic token setup

--- neovim/.config/nvim/lua/plugins/lsp/init.lua
+++ neovim/.config/nvim/lua/plugins/lsp/init.lua
@@ -279,20 +279,7 @@ return {
         },
 
         -- override lsp servers
-        setup = {
-          gopls = function(server, server_opts)
-            Lsp.on_attach(function(client, _)
-              if client.name == 'gopls' and not client.server_capabilities.semanticTokensProvider then
-                local semantic = client.config.capabilities.textDocument.semanticTokens
-                client.server_capabilities.semanticTokensProvider = {
-                  full = true,
-                  legend = { tokenModifiers = semantic.tokenModifiers, tokenTypes = semantic.tokenTypes },
-                  range = true,
-                }
-              end
-            end)
-          end,
-        },
+        setup = {},
       }
     end,
     config = function(_, opts)
