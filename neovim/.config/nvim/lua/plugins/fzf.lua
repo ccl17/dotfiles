6799b57d2d0c5267c54a4399f4936bb91dc4efdb
Samuel <chenlu.ccl@gmail.com>
2024-08-09 00:51:34 +0800

    add fzf-lua mappings

--- neovim/.config/nvim/lua/plugins/fzf.lua
+++ neovim/.config/nvim/lua/plugins/fzf.lua
@@ -8,9 +8,20 @@ return {
   keys = {
     { '<leader>/', function() require('fzf-lua').live_grep() end, desc = 'live grep' },
     { '<leader>/', function() require('fzf-lua').grep_visual() end, mode = 'v', desc = 'grep visual selection' },
+    -- find
     { '<leader>ff', function() require('fzf-lua').files() end, desc = 'Find files' },
     { '<leader>fb', '<cmd>FzfLua buffers sort_mru=true sort_lastused=true<cr>', desc = 'buffers' },
     { '<leader>fo', function() require('fzf-lua').oldfiles() end, desc = 'live grep' },
+    -- git
+    { '<leader>gc', function() require('fzf-lua').git_commits() end, desc = 'git commits' },
+    { '<leader>gs', function() require('fzf-lua').git_status() end, desc = 'git status' },
+    -- search
+    { '<leader>sb', function() require('fzf-lua').grep_curbuf() end, desc = 'grep current buffer' },
+    { '<leader>ss', function() require('fzf-lua').lsp_document_symbols() end, desc = 'lsp document symbols' },
+    { '<leader>sS', function() require('fzf-lua').lsp_live_workspace_symbols() end, desc = 'lsp workspace symbols' },
+    { '<leader>sd', function() require('fzf-lua').diagnostics_document() end, desc = 'lsp document diagnostics' },
+    { '<leader>sD', function() require('fzf-lua').diagnostics_workspace() end, desc = 'lsp workspace diagnostics' },
+    { '<leader>sk', function() require('fzf-lua').keymaps() end, desc = 'Keymaps' },
   },
   config = function()
     local fzf = require('fzf-lua')
