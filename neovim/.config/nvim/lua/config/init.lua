56d38dd67c4cc15f9b224361807d0ba670fcb410
Samuel <chenlu.ccl@gmail.com>
2024-10-07 20:27:17 +0800

    nuke

--- neovim/.config/nvim/lua/config/init.lua
+++
@@ -1,4 +0,0 @@
-require('config/general')
-require('config/mappings')
-require('config/autocmds')
-require('config/plugins')
--- neovim/.config/nvim/lua/plugins/neoscroll.lua
+++
@@ -1,8 +0,0 @@
-return {
-  'karb94/neoscroll.nvim',
-  event = 'VeryLazy',
-  opts = {
-    hide_cursor = true,
-    mappings = { '<C-d>', '<C-u>', 'zt', 'zz', 'zb' }
-  },
-}
