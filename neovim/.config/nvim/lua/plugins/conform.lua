18cacef17344060dfeb9f4babe0fc9c4c605b7c0
Samuel <chenlu.ccl@gmail.com>
2024-12-21 07:09:54 +0800

    format golang files and organize import groups

--- neovim/.config/nvim/lua/plugins/conform.lua
+++ neovim/.config/nvim/lua/plugins/conform.lua
@@ -7,11 +7,13 @@ return {
   config = function()
     require('conform').setup({
       formatters_by_ft = {
+        -- fix imports, then format using lsp (organize import groups)
+        go = { 'goimports', lsp_format = 'last' },
         lua = { 'stylua' },
       },
       format_on_save = function(bufnr)
         if not util.autoformat_enabled(bufnr) then return end
-        return { timeout_ms = 500 }
+        return { timeout_ms = 3000 }
       end,
     })
 
@@ -23,29 +25,5 @@ return {
       { desc = 'Toggle buffer autoformat' }
     )
     vim.keymap.set('n', '<leader>xF', function() util.toggle_autoformat() end, { desc = 'Toggle global autoformat' })
-
-    -- gopls formatting with auto import
-    vim.api.nvim_create_autocmd({ 'BufWritePre', 'BufNewFile' }, {
-      pattern = '*.go',
-      callback = function(args)
-        local params = vim.lsp.util.make_range_params()
-        params.context = { only = { 'source.organizeImports' } }
-        -- buf_request_sync defaults to a 1000ms timeout. Depending on your
-        -- machine and codebase, you may want longer. Add an additional
-        -- argument after params if you find that you have to write the file
-        -- twice for changes to be saved.
-        -- E.g., vim.lsp.buf_request_sync(0, "textDocument/codeAction", params, 3000)
-        local result = vim.lsp.buf_request_sync(0, 'textDocument/codeAction', params)
-        for cid, res in pairs(result or {}) do
-          for _, r in pairs(res.result or {}) do
-            if r.edit then
-              local enc = (vim.lsp.get_client_by_id(cid) or {}).offset_encoding or 'utf-16'
-              vim.lsp.util.apply_workspace_edit(r.edit, enc)
-            end
-          end
-        end
-        require('conform').format({ bufnr = args.buf })
-      end,
-    })
   end,
 }
