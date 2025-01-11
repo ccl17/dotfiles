4b681a1ee530987ed75f7466dd11d26f1d1321f9
Samuel <chenlu.ccl@gmail.com>
2025-01-11 16:57:20 +0800

    add code action keymap

--- neovim/.config/nvim/lua/plugins/fzf.lua
+++ neovim/.config/nvim/lua/plugins/fzf.lua
@@ -20,6 +20,7 @@ return {
     { '<leader>sb', function() require('fzf-lua').lgrep_curbuf() end, desc = 'grep current buffer' },
     { '<leader>ss', function() require('fzf-lua').lsp_document_symbols() end, desc = 'lsp document symbols' },
     { '<leader>sS', function() require('fzf-lua').lsp_live_workspace_symbols() end, desc = 'lsp workspace symbols' },
+    { '<leader>sc', function() require('fzf-lua').lsp_code_action() end, desc = 'lsp code actions' },
     { '<leader>sd', function() require('fzf-lua').diagnostics_document() end, desc = 'lsp document diagnostics' },
     { '<leader>sD', function() require('fzf-lua').diagnostics_workspace() end, desc = 'lsp workspace diagnostics' },
     { '<leader>sk', function() require('fzf-lua').keymaps() end, desc = 'Keymaps' },
