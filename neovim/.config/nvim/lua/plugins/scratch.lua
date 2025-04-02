fdb8843043d81bb1084fe9b8800ae4414a82628e
Samuel <chenlu.ccl@gmail.com>
2025-04-03 01:21:55 +0800

    config scratch

--- neovim/.config/nvim/lua/plugins/scratch.lua
+++ neovim/.config/nvim/lua/plugins/scratch.lua
@@ -1,4 +1,16 @@
 return {
   'LintaoAmons/scratch.nvim',
   event = 'VeryLazy',
+  dependencies = {
+    { 'ibhagwan/fzf-lua' },
+  },
+  opts = {
+    window_cmd = 'rightbelow vsplit',
+    use_telescope = false,
+    file_picker = 'fzflua',
+    filetypes = { 'json', 'markdown' },
+    filetype_details = {
+      json = {},
+    },
+  },
 }
