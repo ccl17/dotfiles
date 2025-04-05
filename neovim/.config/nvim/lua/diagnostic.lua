fa3dc721e823398c3a94e52837c56deb5eaf3290
Samuel <chenlu.ccl@gmail.com>
2025-04-05 20:26:10 +0800

    diagnostic and lint autocmd

--- neovim/.config/nvim/lua/diagnostic.lua
+++ neovim/.config/nvim/lua/diagnostic.lua
@@ -4,4 +4,20 @@ for sev, icon in pairs(diagnostic_icons) do
   vim.fn.sign_define(hl, { text = icon, texthl = hl })
 end
 
-vim.diagnostic.config({ update_in_insert = true, virtual_lines = { current_line = true } })
+vim.api.nvim_create_autocmd('InsertEnter', {
+  pattern = '*',
+  callback = function()
+    vim.diagnostic.config({
+      virtual_lines = false,
+    })
+  end,
+})
+
+vim.api.nvim_create_autocmd({ 'BufReadPost', 'InsertLeave' }, {
+  pattern = '*',
+  callback = function()
+    vim.diagnostic.config({
+      virtual_lines = { current_line = true },
+    })
+  end,
+})
--- neovim/.config/nvim/lua/plugins/nvim-lint.lua
+++ neovim/.config/nvim/lua/plugins/nvim-lint.lua
@@ -30,7 +30,7 @@ return {
     golangcilint.ignore_exitcode = true
 
     local try_lint = util.debounce(200, function() lint.try_lint() end)
-    vim.api.nvim_create_autocmd({ 'BufWritePost', 'BufReadPost', 'InsertLeave', 'TextChanged', 'TextChangedI' }, {
+    vim.api.nvim_create_autocmd({ 'BufWritePost', 'BufReadPost', 'InsertLeave', 'TextChanged' }, {
       group = vim.api.nvim_create_augroup('sc/lint', { clear = true }),
       callback = try_lint,
     })
