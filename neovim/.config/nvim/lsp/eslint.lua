6c757f9d715a9df5c8a6cbb72e5b1e245b967409
Samuel <chenlu.ccl@gmail.com>
2026-09-05 15:43:54 +0800

    eslint settings

--- neovim/.config/nvim/lsp/eslint.lua
+++ neovim/.config/nvim/lsp/eslint.lua
@@ -6,5 +6,34 @@ return {
 	root_markers = { ".eslintrc", ".eslintrc.js", ".eslintrc.json", "eslint.config.js", "eslint.config.mjs" },
 	settings = {
 		validate = "on",
+		packageManager = vim.NIL,
+		useESLintClass = false,
+		experimental = {},
+		codeActionOnSave = {
+			enable = false,
+			mode = "all",
+		},
+		format = true,
+		quiet = false,
+		onIgnoredFiles = "off",
+		rulesCustomizations = {},
+		run = "onType",
+		problems = {
+			shortenToSingleLine = false,
+		},
+		-- nodePath configures the directory in which the eslint server should start its node_modules resolution.
+		-- This path is relative to the workspace folder (root dir) of the server instance.
+		nodePath = "",
+		-- use the workspace folder location or the file location (if no workspace folder is open) as the working directory
+		workingDirectory = { mode = "auto" },
+		codeAction = {
+			disableRuleComment = {
+				enable = true,
+				location = "separateLine",
+			},
+			showDocumentation = {
+				enable = true,
+			},
+		},
 	},
 }
