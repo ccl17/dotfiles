117be6ecabf9c19d6f16b8077ee75fa82951c051
Samuel <chenlu.ccl@gmail.com>
2026-09-20 13:47:53 +0800

    remove

--- neovim/.config/nvim/plugin/blink.lua
+++ neovim/.config/nvim/plugin/blink.lua
@@ -1,16 +1,8 @@
 require("luasnip.loaders.from_vscode").lazy_load()
 local luasnip = require("luasnip")
-local types = require("luasnip.util.types")
 luasnip.setup({
 	history = true,
 	update_events = { "TextChanged", "TextChangedI" },
-	ext_opts = {
-		[types.choiceNode] = {
-			active = {
-				virt_text = { { "<-", "Error" } },
-			},
-		},
-	},
 })
 
 local blink = require("blink.cmp")
